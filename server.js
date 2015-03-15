9f2b8236aa7a908356d16a05327e7768be9327fc
Jude <yutao008@gmail.com>
2015-03-15 18:23:08 +0800

    finished building a restful api chapter

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var User = require('./app/models/user');
 var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/myDatabase');
+
+
+
+
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,9 +30,83 @@ app.get('/', function (req, res) {
 
 var apiRouter = express.Router();
 
+
+apiRouter.use(function(req, res, next) {
+    console.log('Somebody just came to our app!');
+    // we will add more to the middle wire
+    next();
+});
+
 apiRouter.get('/', function (req, res) {
     res.json({message: 'hooray! welcome to our api!'});
 });
+
+
+apiRouter.route('/users')
+    .post( function (req, res) {
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        user.save( function (err) {
+            if (err) {
+                if (err.code == 11000)
+                    return res.json({success: false, message: 'A user with that username already exists. '});
+                else 
+                    return res.send(err);
+            }
+
+            res.json({message: 'User created!'});
+        });
+    })
+    .get( function (req, res) {
+        User.find( function(err, users) {
+            if (err) res.send(err);
+
+            res.json(users);
+            
+        });
+        
+        
+    });
+
+
+apiRouter.route('/users/:user_id')
+    .get( function (req, res) {
+        User.findById( req.params.user_id,  function(err, user) {
+            if (err) res.send(err);
+
+            res.json(user);
+        });
+        
+    })
+    .put( function (req, res) {
+        User.findById( req.params.user_id,  function(err, user) {
+            if (err) res.send(err);
+
+            if (req.body.name) user.name = req.body.name;
+            if (req.body.username) user.username = req.body.username;
+            if (req.body.password) user.password = req.body.password;
+
+            user.save(function(err) {
+                if (err) res.send(err);
+
+                res.json({message: 'User updated!'});
+            });
+
+        });
+        
+    })
+    .delete( function (req, res) {
+        User.remove({
+            _id: req.params.user_id
+        }, function (err, user) {
+            if (err) return res.send(err);
+            res.json({message: 'Successfully deleted !'});
+        });
+        
+    });
 
 
 app.use('/api', apiRouter);
