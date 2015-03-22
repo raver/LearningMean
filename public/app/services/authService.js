6c7325e096e88166e72fc2042c4e75ff95288dfa
Jude <yutao008@gmail.com>
2015-03-22 11:18:30 +0800

    finished chapter Angular authentication

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -3,13 +3,39 @@ angular.module('authService', [])
     var authFactory = {};
 
     // handle login
-    
+    authFactory.login = function (username, password) {
+        return $http.post( '/api/authenticate', {
+            username: username,
+            password: password
+        })
+        .success( function (data) {
+            AuthToken.setToken(data.token);
+            return data;
+        });
+    };
+
+
     // handle logout
 
+    authFactory.logout = function () {
+        AuthToken.setToken();
+    };
     //  check if a user is logged  in
-    
+
+    authFactory.isLoggedIn = function () {
+        if (AuthToken.getToken()) 
+            return true;
+        else
+            return false;
+    };
     // get the user info
-    
+    authFactory.getUser = function () {
+        if (AuthToken.getToken()) 
+            return $http.get('/api/me');
+        else
+            return $q.reject({ message: 'User has no token.'});
+    };
+
     // return auth factory object
     return authFactory;
 
@@ -19,24 +45,45 @@ angular.module('authService', [])
     var authTokenFactory = {};
 
     // get the token
-    
-    
+    authTokenFactory.getToken = function () {
+        return $window.localStorage.getItem('token');
+    };
+
+
     // set the token or clear the token
-    
+    authTokenFactory.setToken = function(token) {
+        if (token) 
+            $window.localStorage.setItem('token', token);
+        else
+            $window.localStorage.removeItem('token');
+    };
+
     return AuthTokenFactory;
-
-
 })
 .factory('AuthInterceptor', function ($q, AuthToken) {
     var interceptorFactory = {};
 
     // attach the token to every request
-    
-    
+    interceptorFactory.request = function (config) {
+        var token = AuthToken.getToken();
+
+        if (token)
+            config.headers['x-access-token'] = token;
+
+        return config;
+    };
+
+    interceptorFactory.responseError = function (response) {
+        if ( response.status == 403 ) {
+            AuthToken.setToken();
+            $location.path('/login');
+        }
+
+        return $q.reject( response );
+    };
+
+
     // redirect if a token doesn't authenticate 
-    
-    
+
     return interceptorFactory;
-
-
 });
